cb7caff052a4164e7e41905a91959f7ac077f6e6
anthony angatia <anthonyangatia@gmail.com>
2022-09-02 15:25:02 +0300

    [Part 1]Project configuration

--- database/postgresql/create_tables.sql
+++ database/postgresql/create_tables.sql
@@ -55,8 +55,8 @@ CREATE TABLE time_slots
 (
     time_slot_id         SERIAL PRIMARY KEY,
     time_slot_date       date                   NOT NULL,
-    start_time           time without time zone NOT NULL,
-    end_time             time without time zone NOT NULL,
+    start_time           VARCHAR  NOT NULL,
+    end_time             VARCHAR  NOT NULL,
     is_keynote_time_slot boolean default false  NOT NULL
 );
 
@@ -96,7 +96,7 @@ CREATE TABLE speakers
     title         varchar(40)   NOT NULL,
     company       varchar(50)   NOT NULL,
     speaker_bio   varchar(2000) NOT NULL,
-    speaker_photo BYTEA   NULL
+    speaker_photo binary   NULL
 );
 
 CREATE TABLE session_speakers
